6c250462246c7ddd6abc126349b34235755f2cbc
thinhvd <20021444@vnu.edu.vn>
2026-05-22 23:35:58 +0700

    first init to github

---
+++ NodeServer/index.js
@@ -0,0 +1,30 @@
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+
+function handler(req, res) {
+    fs.readFile(__dirname + req.url,
+        function (err, data) {
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.of('/model').on('connection', (socket) => {
+    console.log('a model client connected');
+
+    socket.on('result_data', (result) => {
+        if (result != 0) {
+            socket.broadcast.emit('result_download', result);
+        }
+    });
+
+    socket.on('disconnect', () => { console.log('a model client disconnected') });
+});
+
+app.listen(6789, () => console.log('listening on http://127.0.0.1:6789/model.html'));
